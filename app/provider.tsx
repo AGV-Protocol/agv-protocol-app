0b45906703a2be5f78074b002611ee6bccdab6c9
Phantom <posiayoola102@gmail.com>
2025-09-03 03:34:48 +0100

    modified:   app/provider.tsx

--- app/provider.tsx
+++ app/provider.tsx
@@ -22,7 +22,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThirdwebProvider client={client}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           {children}
         </ThemeProvider>
       </ThirdwebProvider>
